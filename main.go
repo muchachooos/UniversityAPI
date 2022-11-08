6cd32b7c3f8d9748322634850cf9d0be53ac4344
muchachooos <odnorogg@bk.ru>
2022-11-08 13:14:10 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import _ "github.com/go-sql-driver/mysql"
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
 
 func main() {
+	router := gin.New()
 
+	_, err := sqlx.Open("mysql", "root:040498usa_wot@tcp(127.0.0.1:3306)/userdata")
+	if err == nil {
+		panic(err)
+		return
+	}
 }
