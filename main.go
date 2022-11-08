477a8b64f94faec63481912b4f1092bb60681c11
muchachooos <odnorogg@bk.ru>
2022-11-08 20:27:18 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"UniversityAPI/handler"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -9,9 +10,18 @@ import (
 func main() {
 	router := gin.New()
 
-	_, err := sqlx.Open("mysql", "root:040498usa_wot@tcp(127.0.0.1:3306)/userdata")
-	if err == nil {
+	dataBase, err := sqlx.Open("mysql", "root:040498usa_wot@tcp(127.0.0.1:3306)/university")
+	if err != nil {
 		panic(err)
 		return
 	}
+
+	server := handler.Server{
+		dataBase,
+	}
+
+	router.POST("/create_student", server.CreateStudentHandler)
+	router.POST("/delete_student", server.DelStudentHandler)
+
+	router.Run("localhost:8080")
 }
