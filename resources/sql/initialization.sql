0990a64ac9db86cf3d993637cc71c5bd49c1a5f2
muchachooos <odnorogg@bk.ru>
2022-11-08 12:42:11 +0400

    Create initialization.sql

---
+++ resources/sql/initialization.sql
@@ -0,0 +1,30 @@
+CREATE TABLE student
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    students_name VARCHAR(20) NOT NULL,
+    last_name     VARCHAR(30) NOT NULL,
+    class_id      VARCHAR(10),
+    room          VARCHAR(10),
+    FOREIGN KEY (class_id) REFERENCES classes (id),
+    FOREIGN KEY (room) REFERENCES rooms (room_number)
+);
+
+CREATE TABLE rooms
+(
+    room_number VARCHAR(10) PRIMARY KEY
+);
+
+CREATE TABLE classes
+(
+    id             VARCHAR(10) PRIMARY KEY,
+    course         INT         NOT NULL,
+    specialization VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE record_book
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id_student INT NOT NULL,
+    FOREIGN KEY (id_student) REFERENCES student (id)
+        ON DELETE CASCADE
+);
