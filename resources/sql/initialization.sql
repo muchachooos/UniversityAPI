70f6d33a0c62d0ece1ac220165156db287c8c0b5
muchachooos <odnorogg@bk.ru>
2022-11-14 16:56:55 +0400

    Update initialization.sql

--- resources/sql/initialization.sql
+++ resources/sql/initialization.sql
@@ -3,11 +3,12 @@ CREATE TABLE student
     id            INT AUTO_INCREMENT PRIMARY KEY,
     first_name    VARCHAR(20) CHECK (first_name != '') NOT NULL,
     last_name     VARCHAR(30) CHECK (last_name != '')  NOT NULL,
-    group_id       VARCHAR(10) CHECK (group_id != ''),
-    room          VARCHAR(10) CHECK (room != ''),
+    group_id      VARCHAR(10) CHECK (group_id != ''),
+    room          VARCHAR(10),
     date_of_birth VARCHAR(10) CHECK (date_of_birth != ''),
     FOREIGN KEY (group_id) REFERENCES `group` (id),
     FOREIGN KEY (room) REFERENCES rooms (room_number)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE rooms
@@ -18,9 +19,10 @@ CREATE TABLE rooms
 
 CREATE TABLE `group`
 (
-    id             VARCHAR(10) CHECK (id != '') PRIMARY KEY,
-    course         INT                                      NOT NULL,
-    specialization VARCHAR(30) CHECK (specialization != '') NOT NULL
+    id               VARCHAR(10) CHECK (id != '') PRIMARY KEY,
+    course           INT                                      NOT NULL,
+    number_of_places INT,
+    specialization   VARCHAR(30) CHECK (specialization != '') NOT NULL
 );
 
 CREATE TABLE record_book
